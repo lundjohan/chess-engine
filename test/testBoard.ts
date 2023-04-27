1b9a7b2bdfed4614b7ce5f285ed7e8df4011f13a
Johan Lund <drancisdrake@gmail.com>
2023-04-27 14:25:12 +0200

    tests neatler commented

--- test/testBoard.ts
+++ test/testBoard.ts
@@ -3,8 +3,11 @@ import {Board} from '../board';
 
 let board:Board = new Board();
 const result = board.newGame('rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2');
-describe('newGame', function () {
-    it('result should contain a board with an en passant as c6', function () {
+describe('newGame() - initiation of Board members', function () {
+    it('should have correct en passant value', function () {
         assert.equal(result.enPassantSq, 'c6');
+    });
+    it('should have set who to play next correctly', function () {
+        assert.equal(result.whiteMoveNext, true);
     });
 });
