883029c1879d7e65941e00cfdfe0db489f13e98a
Johan Lund <drancisdrake@gmail.com>
2023-04-30 21:09:42 +0200

    added passing test

--- test/testBoard.ts
+++ test/testBoard.ts
@@ -87,12 +87,17 @@ describe('moveFEN() - full move number', function () {
     );
 });
 describe('moveFEN() - castling rights', function () {
-    const f = Board.newGame('rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2');
+    //this board is different from the above, as both kings are free to move
+    const f = Board.newGame('rnbqkbnr/pp1p1ppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2');
     it('should have castling rights KQkq', function () {
         assert.equal(f.castlingRights, 'KQkq');
     });
     it('should remove whites castling rights after white king move', function () {
         f.moveFEN('e1e2');
         assert.equal(f.castlingRights, 'kq');
+    });
+    it('should also remove blacks castling rights after black king move', function (){
+        f.moveFEN('e8e7');
+        assert.equal(f.castlingRights, '');
     });
 });
