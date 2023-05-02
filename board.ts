434a1352c027b829f2088f67522430e9dd194377
Johan Lund <drancisdrake@gmail.com>
2023-05-02 20:37:55 +0200

    toFen is passing tests, all tests are passing

--- board.ts
+++ board.ts
@@ -184,29 +184,27 @@ function distanceNorthBetween(from: Square, to: Square): number {
     return (to - from) / 8;
 }
 //prints FEN of state
-export function toFen(board: Board):string {
-    let result:string = "";
-    for (let i = 0; i<board.squares.length; i++) {
+export function toFen(board: Board): string {
+    let result: string = "";
+
+    for (let i = 0; i < board.squares.length; i++) {
         if (i > 0 && i % 8 === 0) {
-            result += '/';
+            //add '/'
+            result += "/";
         }
         let piece = board.squares[i];
-        if (piece === undefined) {
-            result += "1";
-        }
-        else {
-            result += piece;
-        }
+        result += piece === undefined ? "1" : piece;
     }
-    //revert to a8-h1 (internal board goes from a1-h8)
-    result = result.split("").reverse().join("");
-    
-    //replace 11111111 with 8, or 111 with 3, etc.
+
+    // Replace consecutive '1's with their count
     result = result.replace(/1{1,8}/g, match => match.length.toString());
+    result = result.split('/').reverse().join('/');
+
     result += board.whiteMoveNext ? " w " : " b ";
     result += board.castlingRights + " ";
     result += Square[board.enPassantSq] + " ";
     result += board.halfMoveClock + " ";
     result += board.fullMoveNumber;
+
     return result;
 }
