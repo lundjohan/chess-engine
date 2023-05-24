634fb53e722d136574da67c38bea7976c8691f71
Johan Lund <drancisdrake@gmail.com>
2023-05-24 14:32:54 +0200

    remove initiation of en passant square of latest test

--- test/testBoard.ts
+++ test/testBoard.ts
@@ -319,7 +319,7 @@ describe('Chess Board', function () {
         });
     });
     describe('moveFEN()', function () {
-        const b = Board.newGame('k7/8/p7/8/1P6/8/8/K7 w KQkq c6 0 2');
+        const b = Board.newGame('k7/8/p7/8/1P6/8/8/K7 w KQkq - 0 2');
         it('should start this board with halfmoveclock at 0', function () {
             assert.equal(b.halfMoveClock, 0);
         });
